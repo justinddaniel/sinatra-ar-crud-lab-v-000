3e8003106b14cff73d3d71c7b5bbf0cf8dd94f12
Justin Daniel <justinddaniel@gmail.com>
2017-10-22 23:46:07 +0000

    automatically backed up by learn

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,18 +1,18 @@
 require 'pry'
 class Post < ActiveRecord::Base
 
-  #attr_accessor :name, :content
+  attr_accessor :name, :content
 
-  #POSTS = []
+  POSTS = []
 
-  #def initialize(params)
-  #  @name = params[:name]
-#    @content = params[:content]
-#    POSTS << self
-#  end
+  def initialize(params)
+    @name = params[:name]
+    @content = params[:content]
+    POSTS << self
+  end
 
-#  def self.all
-#    POSTS
-#  end
+  def self.all
+    POSTS
+  end
 
 end
