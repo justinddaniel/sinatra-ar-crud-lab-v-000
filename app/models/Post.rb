1073442142ae37feb3e3d662c8cd0f70f661f05f
Justin Daniel <justinddaniel@gmail.com>
2017-10-22 23:29:20 +0000

    automatically backed up by learn

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,17 +1,17 @@
 class Post < ActiveRecord::Base
 
-#  attr_accessor :name, :content
+  attr_accessor :name, :content
 
-#  POSTS = []
+  POSTS = []
 
-#  def initialize(params)
-#    @name = params[:name]
-#    @content = params[:content]
-#    POSTS << self
-#  end
+  def initialize(params)
+    @name = params[:name]
+    @content = params[:content]
+    POSTS << self
+  end
 
-#  def self.all
-#    POSTS
-#  end
+  def self.all
+    POSTS
+  end
 
 end
