141dcecdca978d801ec0e113907b6916450e63cb
Justin Daniel <justinddaniel@gmail.com>
2017-10-23 00:39:26 +0000

    automatically backed up by learn

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,6 +1,10 @@
 require 'pry'
 class Post < ActiveRecord::Base
 
+#This class inherits all its needed methods from ActiveRecord::Base,
+#and its attr_accessor fields from my table migration. Therefore the code
+#below is unneeded and commented out. 
+
 #  attr_accessor :name, :content
 
 #  POSTS = []
